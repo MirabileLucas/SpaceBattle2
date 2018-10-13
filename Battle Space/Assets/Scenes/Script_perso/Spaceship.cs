7fb3e8814fc4c983b7a5af88be5ce86117f641f7
Rachel <rachel.glaise@etu.univ-amu.fr>
2018-10-13 23:09:29 +0200

    Modifications des déplacements
    Modification des déplacements avec l'ajout d'un joystick virtuel.
    Ajout d'un asset permettant d'apprendre à gérer ce joystick

--- Battle Space/Assets/Scenes/Script_perso/Spaceship.cs
+++ Battle Space/Assets/Scenes/Script_perso/Spaceship.cs
@@ -15,8 +15,11 @@ public class Spaceship : MonoBehaviour {
     public float speed;
     public float torque;
     public GameObject player;
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
     private Rigidbody2D rb;
     Camera mainCamera;
+    public Joystick joystick;
     // Use this for initialization
     void Start () {
         mainCamera = Camera.main;
@@ -30,32 +33,42 @@ public class Spaceship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-#if UNITY_STANDALONE || UNITY_EDITOR
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        float angle = 60.0f;
-        
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
 
-        rb.AddForce(transform.up * speed * moveVertical);
-
-        //rb.AddForce(movement * speed);
-        //rb.MoveRotation(angle * speed);
-        //rb.MoveRotation(Mathf.LerpAngle(rb.rotation, 180.0f, 1.0f* Time.deltaTime));
-        rb.AddTorque(transform.position.y * torque * moveHorizontal);
-       
-#endif
-#if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
-
-        if (Input.touchCount == 1) // if there is a touch
+        if (moveVector != Vector3.zero)
         {
-            Touch touch = Input.touches[0];
-            Vector3 touchPosition = mainCamera.ScreenToWorldPoint(touch.position);  //calculating touch position in the world space
-            touchPosition.z = transform.position.z;
-            transform.position = Vector3.MoveTowards(transform.position, touchPosition, 30 * Time.deltaTime);
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+            transform.Translate(moveVector * speed * Time.deltaTime, Space.World);
         }
-#endif
+
+
+
+        /*#if UNITY_STANDALONE || UNITY_EDITOR
+                float moveHorizontal = SimpleInput.GetAxis(horizontalAxis);
+                float moveVertical = SimpleInput.GetAxis(verticalAxis);
+
+                Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+
+                rb.AddForce(transform.up * speed * moveVertical);
+
+                //rb.AddForce(movement * speed);
+                //rb.MoveRotation(angle * speed);
+                //rb.MoveRotation(Mathf.LerpAngle(rb.rotation, 180.0f, 1.0f* Time.deltaTime));
+                rb.AddTorque(transform.position.y * torque * moveHorizontal);
+
+        #endif*/
+        /*#if UNITY_IOS || UNITY_ANDROID //if current platform is mobile, 
+
+                if (Input.touchCount == 1) // if there is a touch
+                {
+                    Touch touch = Input.touches[0];
+                    Vector3 touchPosition = mainCamera.ScreenToWorldPoint(touch.position);  //calculating touch position in the world space
+                    touchPosition.z = transform.position.z;
+                    transform.position = Vector3.MoveTowards(transform.position, touchPosition, 30 * Time.deltaTime);
+                }
+        #endif*/
         transform.position = new Vector2    //if 'Player' crossed the movement borders, returning him back 
                (
                Mathf.Clamp(transform.position.x, borders.minX, borders.maxX),
